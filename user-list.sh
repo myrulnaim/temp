dc77e2e4cd5d7dee6abf246a0dda15b07599ee73
myrulnaim <31527145+myrulnaim@users.noreply.github.com>
2018-02-20 22:50:48 +0800

    Update user-list.sh

--- user-list.sh
+++ user-list.sh
@@ -16,5 +16,4 @@ JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)
 echo "-------------------------------"
 echo "Jumlah akun: $JUMLAH user"
 echo "-------------------------------"
-echo -e "Modified by Aiman Amir"
 echo -e ""
